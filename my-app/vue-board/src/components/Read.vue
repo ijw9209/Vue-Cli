77c2b601c3414fd83c65b1b85ac75a6636bda19e
ijw9209 <ijw9209@naver.com>
2020-02-07 00:14:52 +0900

    Read02

--- my-app/vue-board/src/components/Read.vue
+++ my-app/vue-board/src/components/Read.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
-        Read
+        <table>
+            <tr>
+                <td>글쓴이</td>
+                <td>제목</td>
+                <td>내용</td>
+            </tr>
+            <tr :key="index" v-for="(value,index) in data">
+                <td>{{value.writer}}</td>
+                <td>{{value.title}}</td>
+                <td>{{value.content}}</td>
+            </tr>    
+        </table>
     </div>
 </template>
 <script>
-export default {
+import data from '@/data'
 
+export default {
+ name: 'Read',
+ data() {
+     return {
+        data: data
+     }
+ }
 }
 </script>
