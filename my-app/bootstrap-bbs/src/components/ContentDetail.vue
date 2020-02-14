e521a58a949de5c1eddb179c0972810c37a3d1bd
ijw9209 <ijw9209@naver.com>
2020-02-14 23:44:59 +0900

    css flex

--- my-app/bootstrap-bbs/src/components/ContentDetail.vue
+++ my-app/bootstrap-bbs/src/components/ContentDetail.vue
@@ -69,11 +69,26 @@ export default {
     }
 };
 </script>
+<!-- flex 속성? 주로 레이아웃을 잡을때 사용
+     사용하기위해서는 부모가있고 그안에 자식이 위치하는 상태에서만 사용가능 
+     
+     flex-direction : 자식들의정렬방향을 정함
+                    -row : 수평정렬 방식
+                    -row-reverse
+                    -column : 수직정렬 방식
+                    -column-reverse
+
+      justify-content : 주축을따라 자식들을 정렬하는 방법을 설정가능   
+                     -space-between : 여백을 균등히 배분  
+
+      align-items : 교차축을따라  자식들을 정렬하는 방법을 설정가능                            
+      -->
 <style scoped>
 .content-detail-content-info {
   border: 1px solid black;
   display: flex;
   justify-content: space-between;
+  
 }
 .content-detail-content-info-left {
   width: 720px;
